638290e3d56525ff7e1406e0086b3eca23534b99
Matheus Silva <matheushds.br@gmail.com>
2020-06-04 17:02:06 -0300

    feat(header): add style css

--- header.php
+++ header.php
@@ -3,31 +3,43 @@
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+  <link rel="stylesheet" href="styles/style.css">
   <title>Login System</title>
 </head>
 <body>
 
   <header>
-    <nav>
-      <a href="#">
-        HelloSystem
-        <ul>
-          <li><a href="">Home</a></li>
-          <li><a href="">Portfolio</a></li>
-          <li><a href="">About me</a></li>
-          <li><a href="">Contact</a></li>
+    <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+      <a class="navbar-brand" href="header.php">Yo!</a>
+      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarSupportedContent">
+        <ul class="navbar-nav mr-auto">
+          <li class="nav-item active">
+            <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">Portfolio</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">About me</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">Contact</a>
+          </li>
         </ul>
-      </a>
-      <div class="">
-        <form action="includes/login.inc.php" method="post">
-          <input type="text" name="mailuid" placeholder="Username/Email">
-          <input type="password" name="pwd" placeholder="Password">
-          <button type="submit" name="login-submit">Login</button>
-        </form>
-        <a href="signup.php">Signup</a>
-        <form action="includes/logout.inc.php" method="post">
-          <button type="submit" name="logout-submit">Logout</button>
-        </form>
       </div>
+      <form class="form-inline justify-form">
+        <div class="my-2">
+          <input type="text" placeholder="Usuario/Email" class="mr-2">
+          <input type="text" placeholder="Password">
+        </div>
+        <div>
+          <button class="btn btn-outline-success mx-sm-2" type="button">Fazer Login</button>
+          <button class="btn btn-outline-success" type="button">Cadastre-se</button>
+        </div>
+      </form>
     </nav>
   </header>
