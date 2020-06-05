1d0d98f21369307bc1b965c4dea9c034598954da
Matheus Silva <matheushds.br@gmail.com>
2020-06-04 23:12:03 -0300

    feat(index,login) add login

--- index.php
+++ index.php
@@ -4,8 +4,14 @@
 
 <main>
   <div class="p-position my-2">
-    <p>You're logged out!</p>
-    <p>You're logged in!</p>
+    <?php
+      if (isset($_SESSION['userId'])) {
+        echo '<p class="text-success">You are logged in!</p>';
+      }
+      else {
+        echo '<p class="text-success">You are logged out!</p>';
+      }
+    ?>
   </div>
 </main>
 
